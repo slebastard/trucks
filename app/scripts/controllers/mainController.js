204878afa33fd716336b57e5ed143e1910184d31
Arnaud Paran <paran.arnaud@gmail.com>
2016-05-03 10:22:56 +0200

    Initial commit

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Trucks.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('Trucks')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Trucks.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('Trucks')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
