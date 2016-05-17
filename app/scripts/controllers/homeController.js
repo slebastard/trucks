facc8143b21a6da98667b5fa6438d556f8b24312
Arnaud Paran <paran.arnaud@gmail.com>
2016-05-17 22:01:42 +0200

    example for simon

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -7,21 +7,11 @@
  * # HomeController
  */
 angular.module('Trucks')
-  .controller('HomeController', function($scope, ExampleService) {
+  .controller('HomeController', function($scope, $http) {
 
-    $scope.myHTML = null;
-
-    // just an example...
-    $scope.fetchRandomText = function() {
-      ExampleService.doSomethingAsync()
-        .then(ExampleService.fetchSomethingFromServer)
-        .then(function(response) {
-          $scope.myHTML = response.data.text;
-          // close pull to refresh loader
-          $scope.$broadcast('scroll.refreshComplete');
-        });
-    };
-
-    $scope.fetchRandomText();
-
+    $scope.myHTML = {'1': '2'};
+    $http.get('http://0.0.0.0:5000/restaurants/restaurants.json')
+      .then(function(response) {
+        $scope.myHTML = response.data.data[0];
+      });
   });
