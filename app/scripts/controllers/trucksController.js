da158c3c321bdd1e42d5592a892989e142c135c2
Arnaud Paran <paran.arnaud@gmail.com>
2016-05-21 18:09:44 +0200

    imagesUrls added in restaurant description

--- app/scripts/controllers/trucksController.js
+++ app/scripts/controllers/trucksController.js
@@ -12,15 +12,57 @@ angular.module('Trucks')
     $scope.restaurants = [
       {
         id: 0,
-        name: 'restaurant 1'
+        name: 'restaurant 1',
+        dishes: [
+        {
+          id: 0,
+          name: 'Plat 1',
+          description: 'description',
+          imageUrl: 'https://static.pexels.com/photos/5938/food-salad-healthy-lunch.jpg'
+        },
+        {
+          id: 1,
+          name: 'Plat 2',
+          description: 'description',
+          imageUrl: 'https://static.pexels.com/photos/5938/food-salad-healthy-lunch.jpg'
+        }
+        ]
       },
       {
         id: 1,
-        name: 'restaurant 2'
+        name: 'restaurant 2',
+        dishes: [
+        {
+          id: 0,
+          name: 'Plat 1',
+          description: 'description',
+          imageUrl: 'https://static.pexels.com/photos/5938/food-salad-healthy-lunch.jpg'
+        },
+        {
+          id: 1,
+          name: 'Plat 2',
+          description: 'description',
+          imageUrl: 'https://static.pexels.com/photos/5938/food-salad-healthy-lunch.jpg'
+        }
+        ]
       },
       {
         id: 2,
-        name: 'restaurant 3'
+        name: 'restaurant 3',
+        dishes: [
+        {
+          id: 0,
+          name: 'Plat 1',
+          description: 'description',
+          imageUrl: 'https://static.pexels.com/photos/5938/food-salad-healthy-lunch.jpg'
+        },
+        {
+          id: 1,
+          name: 'Plat 2',
+          description: 'description',
+          imageUrl: 'https://static.pexels.com/photos/5938/food-salad-healthy-lunch.jpg'
+        }
+        ]
       }
     ];
     // do something with $scope
