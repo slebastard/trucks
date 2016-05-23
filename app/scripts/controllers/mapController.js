1fc09cfc8825e522b849fd4a137167242774e91b
Arnaud Paran <paran.arnaud@gmail.com>
2016-05-23 18:18:37 +0200

    good & tasty pin added

--- app/scripts/controllers/mapController.js
+++ app/scripts/controllers/mapController.js
@@ -15,9 +15,27 @@ angular.module('Trucks')
     document.addEventListener('deviceready', function() {
       // Define a div tag with id='map_canvas'
       mapDiv = document.getElementById('map_canvas');
+      var GOOD_TASTY = new plugin.google.maps.LatLng(48.8415978, 2.584430099999963);
 
       // Initialize the map plugin
-      map = plugin.google.maps.Map.getMap(mapDiv);
+      map = plugin.google.maps.Map.getMap(mapDiv, {
+        'camera': {
+          'latLng': GOOD_TASTY,
+          'zoom': 17
+        }
+      });
+      map.addEventListener(plugin.google.maps.event.MAP_READY, function() {
+
+        map.addMarker({
+          'position': GOOD_TASTY,
+          'title': 'Good & Tasty !'
+        }, function(marker) {
+
+          marker.showInfoWindow();
+
+        });
+
+      });
     });
 
     $scope.goBack = function() {
